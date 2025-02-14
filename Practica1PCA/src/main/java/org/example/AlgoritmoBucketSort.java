2cb129bf545a75cc00654e8bf03276207026644e
Rober <robermat1604@gmail.com>
2025-02-14 09:27:54 +0100

    Commit Inicial

---
+++ Practica1PCA/src/main/java/org/example/AlgoritmoBucketSort.java
@@ -0,0 +1,11 @@
+package org.example;
+import practicas_pca.TesterRun;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgoritmoBucketSort implements TesterRun{
+    @Override
+    public ArrayList<Integer> bucketSort(List<Integer> numbers, int num_threads) {
+        /* CODIGO A COMPLETAR POR EL ALUMNO */
+    }
+}
