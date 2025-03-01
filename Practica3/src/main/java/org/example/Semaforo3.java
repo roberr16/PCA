4dc027ffde05b9c338571d3e626c877cad9f5dba
bs0455 <ivan.medina@alumnos.upm.es>
2025-03-01 21:05:24 +0100

    Clase Semaforo3 terminada.

--- Practica3/src/main/java/org/example/Semaforo3.java
+++ Practica3/src/main/java/org/example/Semaforo3.java
@@ -1,27 +1,23 @@
-package P3;
+package org.example;
 
 import java.util.concurrent.Semaphore;
 
 public class Semaforo3 {
-    //COMPLETAR EL CÓDIGO
-    //CONFIGURAR LOS SEMÁFOROS
-    //private final Semaphore semaforo1 = new Semaphore(XX); // Bloquea el hilo 1
-    //private final Semaphore semaforo2 = new Semaphore(XX); // Bloquea el hilo 2
-    //private final Semaphore semaforo3 = new Semaphore(XX); // Bloquea el hilo 3
+
+    private final Semaphore semaforo1 = new Semaphore(1); // Bloquea el hilo 1
+    private final Semaphore semaforo2 = new Semaphore(0); // Bloquea el hilo 2
+    private final Semaphore semaforo3 = new Semaphore(0); // Bloquea el hilo 3
 
     private final Runnable tarea = () -> {
         String nombreHilo = Thread.currentThread().getName();
 
         try {
             if (nombreHilo.equals("Hilo 1")) {
-                //COMPLETAR EL CÓDIGO
-                //comprobar bloqueo
+                semaforo1.acquire();
             } else if (nombreHilo.equals("Hilo 2")) {
-                //COMPLETAR EL CÓDIGO
-                //comprobar bloqueo
+                semaforo2.acquire();
             } else if (nombreHilo.equals("Hilo 3")) {
-                //COMPLETAR EL CÓDIGO
-                //comprobar bloqueo
+                semaforo3.acquire();
             }
 
             System.out.println(nombreHilo + " iniciando...");
@@ -29,14 +25,11 @@ public class Semaforo3 {
             System.out.println(nombreHilo + " terminado.");
 
             if (nombreHilo.equals("Hilo 1")) {
-                //COMPLETAR EL CÓDIGO
-                //desbloquear
+                semaforo2.release();
             } else if (nombreHilo.equals("Hilo 2")) {
-                //COMPLETAR EL CÓDIGO
-                //desbloquear
-            }else if (nombreHilo.equals("Hilo 3")) {
-                //COMPLETAR EL CÓDIGO
-                //desbloquear
+                semaforo3.release();
+            } else if (nombreHilo.equals("Hilo 3")) {
+                semaforo1.release();
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -48,7 +41,7 @@ public class Semaforo3 {
             Thread hilo1 = new Thread(tarea, "Hilo 1");
             Thread hilo2 = new Thread(tarea, "Hilo 2");
             Thread hilo3 = new Thread(tarea, "Hilo 3");
-    
+
             hilo1.start();
             hilo2.start();
             hilo3.start();
